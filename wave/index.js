bddded95a3216ce280724542b1c4b509d1101912
kazutok <36791609+kazutok@users.noreply.github.com>
2018-03-21 21:32:49 +0900

    Update index.js

--- wave/index.js
+++ wave/index.js
@@ -87,8 +87,7 @@
       ctx.fill();
 
       ctx.fillStyle = "#0000ff"; //blue
-      ctx.rect(0, canvas.height*(1 - (ave+rms)/255), canvas.width, 1);
-      ctx.fill();
+      ctx.fillrect(0, canvas.height*(1 - (ave+rms)/255), canvas.width, 1);
       
       ctx.fillStyle = "#000000"; //black
       ctx.font = "12px Arial";
