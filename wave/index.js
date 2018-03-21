cb79e4921240af6ef2f9aa44e6d9d77c67f960e5
kazutok <36791609+kazutok@users.noreply.github.com>
2018-03-21 18:40:27 +0900

    Update index.js

--- wave/index.js
+++ wave/index.js
@@ -31,20 +31,23 @@
         data   = new Uint8Array(LENGTH),
         w      = 0,
         i      = 0,
+        f      = 0,
         count  = 0;
     
 //    alert(audioCtx.sampleRate + " Hz");
+    f = audioCtx.sampleRate;
     btn.classList.add("off");
     analyser.fftSize = LENGTH;
     src.connect(analyser);
 //    alert("fft start");
 
-      ctx.fillStyle = "#ff0000"; //DARK RED
-      ctx.font = "20px Arial";
 
     setInterval(() => {
       canvas.width  = window.innerWidth;
       canvas.height = window.innerHeight;
+
+      ctx.fillStyle = "#ff0000"; //DARK RED
+      ctx.font = "20px Arial";
 
       w = canvas.width / LENGTH,
 
@@ -53,18 +56,20 @@
 //      data2 = data1;
       analyser.getByteTimeDomainData(data);
 //      data = data3.concat(data2).concat(data1);
-      
-      
+            
       for (i = 0; i < LENGTH; i++) {
-        ctx.rect(i * w, canvas.height*(1 - data[i]/255/2), w, canvas.height*data[i]/255/2);
+        ctx.rect(i * w, canvas.height*(1 - data[i]/255), w, canvas.height*data[i]/255);
 //        ctx.rect(i * w, canvas.height*(1 - data[i]/255), w, 5);
         if(data[i] > 200){
           count++;
         }
       }
-
       ctx.fill();
-      ctx.fillText("PEAK COUNT: " + count + "  w:"+canvas.width+" h:"+canvas.height, 10, 25);
+      ctx.fillText("SamplingRate:" + f + "Hz, w:" + canvas.width + ", h:" + canvas.height, 10, 25);
+      ctx.fillText("RMS:" + "  PEAK COUNT: " + count, 35, 25);
+      
+      
+      
     }, 20);
   }
 
